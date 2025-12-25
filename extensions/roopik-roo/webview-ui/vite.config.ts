96c7d817745346f84b56fc11e7fc492a1ea11d10
Dinesh Yadav <13635627+HumbleBee14@users.noreply.github.com>
2025-12-25 12:24:31 -0500

    packages

--- extensions/roopik-roo/webview-ui/vite.config.ts
+++ extensions/roopik-roo/webview-ui/vite.config.ts
@@ -53,9 +53,9 @@ const persistPortPlugin = (): Plugin => ({
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-	let outDir = "../src/webview-ui/build"
+	let outDir = "./build"
 
-	const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "src", "package.json"), "utf8"))
+	const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "package.json"), "utf8"))
 	const gitSha = getGitSha()
 
 	const define: Record<string, any> = {
