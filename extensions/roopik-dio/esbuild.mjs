b91479588ce7452021f2f3ffbc4e355d5e9e90cd
Dinesh Yadav <13635627+HumbleBee14@users.noreply.github.com>
2025-12-25 02:51:26 -0500

    changed webview to webview-ui

--- extensions/roopik-dio/esbuild.mjs
+++ extensions/roopik-dio/esbuild.mjs
@@ -131,7 +131,7 @@ async function main() {
 							["CHANGELOG.md", "CHANGELOG.md"],
 							["LICENSE", "LICENSE"],
 							["node_modules/vscode-material-icons/generated", "assets/vscode-material-icons"],
-							["webview/audio", "webview-ui/audio"],
+							["webview-ui/audio", "webview-ui/audio"],
 						],
 						rootDir,
 						distDir,
