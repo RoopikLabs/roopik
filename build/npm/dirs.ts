20aba2a30d4d855b062dabe47e1514c7ded9541c
Dinesh Yadav <13635627+HumbleBee14@users.noreply.github.com>
2026-03-31 16:03:01 -0400

    Update dirs.ts

--- build/npm/dirs.ts
+++ build/npm/dirs.ts
@@ -61,7 +61,6 @@ export const dirs = [
 	'.vscode/extensions/vscode-selfhost-import-aid',
 	'.vscode/extensions/vscode-selfhost-test-provider',
 	'.vscode/extensions/vscode-extras',
-	'extensions/roopik-roo',
 ];
 
 if (existsSync(`${import.meta.dirname}/../../.build/distro/npm`)) {
